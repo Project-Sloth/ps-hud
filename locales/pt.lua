e1ee708d62fd32270145dd514cbf756c6ac51cae
RC <82515416+LuquinhasVulgoRC@users.noreply.github.com>
2022-06-13 03:15:28 -0300

    pt.lua correction

--- locales/pt.lua
+++ locales/pt.lua
@@ -1,21 +1,21 @@
 local Translations = {
     notify = {
-        ["hud_settings_loaded"] = "Definições Do HUD Carregadas!",
-        ["hud_restart"] = "O HUD Está A Reiniciar!",
-        ["hud_start"] = "O HUD Está Agora Iniciada!",
-        ["hud_command_info"] = "Este comando reseta as definições do teu HUD!",
-        ["load_square_map"] = "A Carregar Mapa Quadrado...",
-        ["loaded_square_map"] = "Mapa Quadrado Carregado!",
-        ["load_circle_map"] = "A Carregar Mapa Redondo...",
-        ["loaded_circle_map"] = "Mapa Redondo Carregado!",
-        ["cinematic_on"] = "Modo Cinematic Ativado!",
-        ["cinematic_off"] = "Modo Cinematic Desativado!",
-        ["engine_on"] = "Motor Ligado!",
-        ["engine_off"] = "Motor Desligado!",
-        ["low_fuel"] = "Nível Do Óleo Em Baixo!",
-        ["access_denied"] = "Não Estás Autorizado!",
-        ["stress_gain"] = "A Sentir-me Mais Stressado(a)!",
-        ["stress_removed"] = "A Sentir-me Mais Calmo(a)!"
+        ["hud_settings_loaded"] = "Configurações da HUD carregadas!",
+        ["hud_restart"] = "A HUD está reiniciando!",
+        ["hud_start"] = "A HUD está agora iniciada!",
+        ["hud_command_info"] = "Este comando redefine suas configurações atuais da HUD!",
+        ["load_square_map"] = "Carregando mapa quadrado...",
+        ["loaded_square_map"] = "O mapa quadrado foi carregado!",
+        ["load_circle_map"] = "Carregando Mapa do Círcular...",
+        ["loaded_circle_map"] = "O Mapa Círcular foi Carregado!",
+        ["cinematic_on"] = "Modo Cinematográfico Ativado!",
+        ["cinematic_off"] = "Modo cinematográfico desativado!",
+        ["engine_on"] = "Motor ligado!",
+        ["engine_off"] = "Motor desligado!",
+        ["low_fuel"] = "Nível de combustível baixo!",
+        ["access_denied"] = "Você não está autorizado!",
+        ["stress_gain"] = "Sentindo-se mais estressado!",
+        ["stress_removed"] = "Sentindo-se mais relaxado!"
     }
 }
 Lang = Locale:new({phrases = Translations, warnOnMissing = true})
