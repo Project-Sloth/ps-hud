92da4f30c092002e79a89c9ab44dd5b98528f92e
SkrippyBoi <110511952+SkrippyBoi@users.noreply.github.com>
2022-09-15 17:44:22 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.Locale = 'en' -- Use Search function here in config.lua. Search for "Loca
 -- Languages default by Project Sloth: ar, cs, da, de, en, es, fa, fi, fr, ge, it, nl, pl, pt, sk, sv, tr
 
 Config.OpenMenu = 'I' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-Config.CruiseControl = 'Y' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
+Config.CruiseControl = 'CAPITAL' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 Config.StartEngine = 'G' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 Config.ToggleSeatbelt = 'B' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
 Config.StressChance = 0.1 -- Default: 10% -- Percentage Stress Chance When Shooting (0-1)
