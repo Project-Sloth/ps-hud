69ea69572d3d2b1ba3205360333342a0f5cf50d6
Gabriel Filipe Guimaraes de Freitas <ggfto@outlook.com>
2024-04-13 09:31:22 -0300

    Permit configure vehicle classes to not gain stress

--- config.lua
+++ config.lua
@@ -139,3 +139,27 @@ Config.FuelBlacklist = {
 	"khamelion",
 	"wheelchair",
 }
+
+Config.VehClassStress = { -- Enable/Disable gaining stress from vehicle classes in this table
+    ['0'] = true, -- Compacts
+    ['1'] = true, -- Sedans
+    ['2'] = true, -- SUVs
+    ['3'] = true, -- Coupes
+    ['4'] = true, -- Muscle
+    ['5'] = true,  -- Sports Classics
+    ['6'] = true, -- Sports
+    ['7'] = true, -- Super
+    ['8'] = false, -- Motorcycles
+    ['9'] = true, -- Off Road
+    ['10'] = true, -- Industrial
+    ['11'] = true,  -- Utility
+    ['12'] = true,  -- Vans
+    ['13'] = false, -- Cycles
+    ['14'] = false, -- Boats
+    ['15'] = false, -- Helicopters
+    ['16'] = false, -- Planes
+    ['18'] = false, -- Emergency
+    ['19'] = false, -- Military
+    ['20'] = false, -- Commercial
+    ['21'] = false  -- Trains
+}
